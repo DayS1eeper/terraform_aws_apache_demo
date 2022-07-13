7a8a523ed1d97bd38a333e38c92e135cbb17d983
Vladyslav Mihun <mihunvlad@gmail.com>
2022-07-13 12:19:08 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,7 +95,6 @@ output "Webserver-Public-IP" {
 resource "aws_instance" "webserver" {
   ami                         = data.aws_ssm_parameter.webserver-ami.value
   instance_type               = "t3.micro"
-  key_name                    = aws_key_pair.webserver-key.key_name
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.sg.id]
   subnet_id                   = aws_subnet.subnet.id
